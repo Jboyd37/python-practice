1932db441f92c1cc60b60fca4aecd946edb9d887
Jboyd37 <nosyaj7@aol.com>
2026-05-16 19:20:36 -0500

    Added profile user input project

---
+++ profile.py
@@ -0,0 +1,14 @@
+# User Input
+
+name = input("Enter your name:")
+age = int(input("Enter your age:"))
+fav_movie = input("Enter your favorite movie:")
+fav_food = input("Enter your favorite food:")
+
+print("")
+print("-----About Me-----")
+
+print(name)
+print(age)
+print(fav_movie)
+print(fav_food)
