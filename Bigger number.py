5cd5aed3907f2c116335dcf01d85565c7e39641c
Jason Boyd <nosyaj7@aol.com>
2026-05-29 14:38:55 -0400

    Added bigger number challenge

---
+++ Bigger number.py
@@ -0,0 +1,13 @@
+# Bigger number
+
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+
+if num1 > num2:
+    print(num1, "is bigger")
+
+elif num2 > num1:
+    print(num2, "is bigger")
+
+else:
+    print("They are equal")
