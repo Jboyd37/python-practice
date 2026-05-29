fa9ec329697e9ecbd9db4b2d4f99f40406fa99ce
Jason Boyd <nosyaj7@aol.com>
2026-05-29 14:37:19 -0400

    Added age calculator project

---
+++ age_calculator.py
@@ -0,0 +1,10 @@
+# Age Calculator
+
+birth_year = int(input("What year were you born? "))
+current_year = 2026
+
+age = current_year - birth_year
+future_age = age + 10
+
+print("You are", age, "years old.")
+print("In 10 years you will be", future_age, "years old.")
